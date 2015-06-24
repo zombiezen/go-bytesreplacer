5e8b09781f1588a248392668e3ebf73aa3968acb
Ross Light <light@google.com>
2015-06-24 09:44:01 -0700

    add custom import path and package doc

--- replace.go
+++ replace.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package bytesreplacer
+// Package bytesreplacer provides a utility for replacing parts of byte slices.
+package bytesreplacer // import "zombiezen.com/go/bytesreplacer"
 
 import (
 	. "bytes"
